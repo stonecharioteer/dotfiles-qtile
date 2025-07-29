1f770fdd6cd050587dadf4d0e34589ea9ef1d91f
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2025-07-29 20:22:16 +0530

    fix: update autostart and picom

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,7 @@
 export XCURSOR_THEME=Adwaita
 export XCURSOR_SIZE=24
 # only run once
-pgrep -x picom >/dev/null || picom --config ~/.config/picom.conf &
+pgrep -x picom >/dev/null || picom --config ~/.config/picom.conf --no-use-damage --daemon
 pgrep -x nm-applet >/dev/null || nm-applet &
 pgrep -x pasystray >/dev/null || pasystray &
 pgrep -x dunst >/dev/null || dunst &
@@ -26,6 +26,7 @@ systemctl --user enable unlock-on-resume.service
 pgrep -f "touchegg$" >/dev/null || touchegg &
 
 # Start Conky in the background after a delay
+export ACTIVE_INTERFACE=$(ip route get 8.8.8.8 | awk '{print $5}')
 pgrep -x conky >/dev/null || (conky | logger -t conky) &
 
 notify-send "Qtile" "Config loaded successfully." -u low 2>/dev/null
